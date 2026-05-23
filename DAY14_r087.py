2d6265ebc555c3d9ef07cea5931bd75f95ba6b66
rknd24 <rknd24>
2026-05-23 23:18:47 +0900

    DAY3総復習

--- DAY14_r087.py
+++ DAY14_r087.py
@@ -2,17 +2,11 @@ a = int(input())
 b = int(input())
 c = int(input())
 x = int(input())
-a_count = 0
-b_count = 0
-c_count = 0
-while x % 500 >= 500 and a!= 0:
-    a_count += 1
-    a -= 1
-while x % 100 >= 100 and b!= 0:
-    b_count += 1
-    b -= 1
-while x % 50 >= 50 and c!= 0:
-    c_count += 1
-    c -= 1
-print(a_count + b_count + c_count)
+count = 0
+for i in range(a+1):
+    for j in range(b+1):
+        for k in range(c+1):
+            if 500*i +100*j + 50*k == x:
+                count += 1
+print(count)
 
