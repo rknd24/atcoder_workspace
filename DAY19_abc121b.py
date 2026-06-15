3c20174bba2b43e8663bb306c20ef24f2b68e419
rknd24 <rknd24>
2026-06-15 18:00:13 +0900

    feat(abc121b): 配列要素数に対するrange(m)の適切なループ範囲を実装

---
+++ DAY19_abc121b.py
@@ -0,0 +1,18 @@
+n,m,c = map(int,input().split())
+b = list(map(int,input().split()))
+count = 0 #1以上の時のcount用
+for _ in range(n):
+    a = list(map(int,input().split()))
+    ans = c
+    for i in range(m):
+        ans += a[i]*b[i]
+    if ans > 0:
+        count += 1
+print(count)
+
+
+
+
+
+
+
