a50dcc3a13fabd25533cf32cc2383158d41e6a08
コンドウリク <konriku.170824@gmail.com>
2026-06-18 16:55:16 +0900

    DAY20review

---
+++ DAY21_YR_review.py
@@ -0,0 +1,8 @@
+a,b = map(int,input().split())
+s = input()
+ 
+ls = s.split("-")
+if ls[0].isdigit() and ls[1].isdigit() and s[a] == "-" and len(ls) == 2 and len(s) == a+b+1:
+    print("Yes")
+else:
+    print("No")    
