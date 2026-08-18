e6d48353a640eba5476f94be3fc0cfb093b4defd
コンドウリク <konriku.170824@gmail.com>
2026-08-18 14:45:38 +0900

    feat: 067 Base 8 to 9 AC、9進数への基数変換とdequeでの桁管理を実装

---
+++ DAY33_t90_067.py
@@ -0,0 +1,27 @@
+from collections import deque
+n,k = map(int,input().split())
+str_n = str(n)
+new_n = int(str_n,8)
+for _ in range(k):
+    idxs = []
+    remque = deque()
+    while new_n >= 9:
+        rem = new_n % 9
+        remque.appendleft(rem)
+        new_n //= 9
+        if new_n == 8:
+            new_n = 5
+    for i in range(len(remque)):
+        if remque[i] == 8:
+            idxs.append(i)
+            remque[i] = 5
+    ans = str(new_n)
+    for j in remque:
+        ans += str(j)
+        new_n = int(ans,8)
+print(ans)
+    
+
+
+            
+        
